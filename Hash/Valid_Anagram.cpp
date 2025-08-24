0274942f451ae7eebd2e4ebfe7adb72b358cb89f
Eyad Aqrabawi <e.ibrahim@prema-consulting.net>
2025-08-24 11:52:09 +0300

    Fix typo in length check and improve isAnagram function implementation

--- Hash/Valid_Anagram.cpp
+++ Hash/Valid_Anagram.cpp
@@ -1,8 +1,30 @@
+#include <string>
+#include <unordered_map>
+
+using namespace std;
 class Solution {
 public:
+// if this question only requires english letters we can use a fixed array for the general solution its better to use the unordered_map -> hashTable
     bool isAnagram(string s, string t) {
-        if (s.length() != t.lenght())
+
+        if (s.length() != t.length())
             return false;
-        
+
+        unordered_map<char, int> hashTable;
+
+        for (char c : s)
+        {
+            hashTable[c]++;
+        }
+
+        for (char c : t)
+        {
+            if (hashTable.find(c) == hashTable.end() || (hashTable[c] == 0 ))
+                return false;
+                
+            hashTable[c]--;
+        }
+
+        return true;
     }
 };
